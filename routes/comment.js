7828677bfbf671c32fbfa6ca4f1af8e42d2f7661
dong1hyun <limd1238@naver.com>
2022-12-06 14:20:25 +0900

    123

--- routes/comment.js
+++ routes/comment.js
@@ -7,20 +7,51 @@ const { isLoggedIn } = require('./helpers');
 
 const router = express.Router();
 
-    router.post('/commenting/:id', isLoggedIn, async (req, res, next) => {
-        const { comments } = req.body;
-        const postId = req.params.id;
-        try {
-            await Comment.create({
-                comments,
-                userId: req.user.id,
-                postId
-            });
-            res.redirect(`/post/${postId}`);
-        } catch (err) {
-            console.error(err);
-            next(err);
-        }
-    })
+router.post('/commenting/:id', isLoggedIn, async (req, res, next) => {
+    const { comments } = req.body;
+    const postId = req.params.id;
+    try {
+        await Comment.create({
+            comments,
+            userId: req.user.id,
+            postId
+        });
+        res.redirect(`/post/${postId}`);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.post('/update/:id', async (req, res, next) => {
+    const { comments } = req.body;
+    try {
+        const result = await Post.update({
+            content
+        }, {
+            where: { id: req.params.id }
+        });
+        if (result) res.redirect('/');
+        else next('Not updated!')
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.get('/delete/:id', async (req, res, next) => {
+    try {
+        const result = await Post.destroy({
+            where: { id: comment }
+        });
+
+        if (result) res.redirect('/');
+        else next('Not deleted!')
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
 
 module.exports = router;
